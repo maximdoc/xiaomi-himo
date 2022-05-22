e82b64ffcc6b43119c245e29e85498cdd504549a
maximdoc <maximdoc2000@gmail.com>
2022-05-22 10:00:20 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    $('.top_slider, .slider_items').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        arrows: false
+    });
+
+});
